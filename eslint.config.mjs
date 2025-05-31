0a3960c68e14fd174d6785d4fe0d1791648188e3
Ako-Mawlood <ako.mawlood01@gmail.com>
2025-05-31 05:34:34 +0300

    Add lint and pretier with github actions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,34 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
+import eslintPluginUnusedImports from "eslint-plugin-unused-imports";
+import unicorn from "eslint-plugin-unicorn";
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: process.cwd(),
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends("next/core-web-vitals", "next/typescript",  "plugin:prettier/recommended" ),
+  {
+    plugins: {
+      "unused-imports": eslintPluginUnusedImports,
+      "unicorn":unicorn
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "no-template-curly-in-string": "error",
+      "unused-imports/no-unused-imports": "error",
+      "no-duplicate-imports": "error",
+      "quotes": ["error", "double", { avoidEscape: true }],
+      "react/jsx-key": "error",
+      "react/jsx-no-duplicate-props": "error",
+      "react/jsx-pascal-case": "error", 
+      "no-console": "warn",
+      "no-debugger": "warn",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        { vars: "all", varsIgnorePattern: "^_", argsIgnorePattern: "^_" },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
