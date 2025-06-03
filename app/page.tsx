10f54a59afdbc55f5ce9aec598ac13d7be766df7
Ako-Mawlood <ako.mawlood01@gmail.com>
2025-06-04 00:10:27 +0300

    Remove discription section

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,10 @@
 import { gsap } from "gsap"
 import Hero from "@/components/Hero"
 import TCanvas from "@/components/three/TCanvas"
-import AnimatedText from "@/components/AnimatedText"
+import Projects from "@/components/Projects"
 import clsx from "clsx"
 import { ScrollSmoother } from "gsap/ScrollSmoother"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
-
 gsap.registerPlugin(ScrollSmoother, ScrollTrigger)
 
 export default function HomePage() {
@@ -15,6 +14,7 @@ export default function HomePage() {
     wrapper: "#smooth-wrapper",
     content: "#smooth-content",
   })
+
   return (
     <div id="smooth-wrapper" className="relative">
       <div
@@ -26,14 +26,15 @@ export default function HomePage() {
           <TCanvas />
         </div>
       </div>
-      <h1 className="relative mt-96 p-5 text-8xl md:p-10">
+      {/* <h1 className="relative mt-96 p-5 text-8xl md:p-10">
         <AnimatedText
           text="I build thoughtful, user-friendly front-end interfaces with care and precision."
           delay={0.5}
           isOnScroll={true}
           duration={0.5}
         />
-      </h1>
+      </h1> */}
+      <Projects />
       <section className="h-screen w-screen"></section>
       <section className="h-screen w-screen"></section>
       <section className="h-screen w-screen"></section>
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,11 @@
+"use client"
+
 import React from "react"
 
 export default function Project() {
-  return <div>Project</div>
+  return (
+    <section className="relative mt-[50vh] w-full p-5 md:p-10">
+      <h1 className="mx-auto w-fit text-8xl font-bold">Selected Works</h1>
+    </section>
+  )
 }
